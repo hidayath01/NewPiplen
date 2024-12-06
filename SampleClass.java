187b7491d042f51ddaf8aafc48f34b1ed6a61795
hidayath01 <hedayath.abdul@qapitol.com>
2024-12-06 20:42:10 +0530

    Second Commit in this project

---
+++ SampleClass.java
@@ -0,0 +1,12 @@
+package TestCases;
+import org.testng.annotations.Test;
+
+public class SampleClass {
+	
+	@Test
+	public void sampleMethod()
+	{
+		System.out.println("Class two Method");
+	}
+
+}
